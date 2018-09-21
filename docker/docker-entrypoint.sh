5fba099691b1db345ee296a55422f8321c10fcf1
gmanipon <gmanipon@jpl.nasa.gov>
2018-09-21 23:14:47 +0000

    start supervisord

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+# get group id
+GID=$(id -g)
+
+# update user and group ids
+gosu 0:0 groupmod -g $GID ops 2>/dev/null
+gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
+gosu 0:0 usermod -aG docker ops 2>/dev/null
+
+# update ownership
+gosu 0:0 chown -R $UID:$GID /home/ops 2>/dev/null || true
+gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+gosu 0:0 chown -R $UID:$GID /var/log/supervisor 2>/dev/null || true
+
+# source verdi virtualenv
+if [ -e "/home/ops/verdi/bin/activate" ]; then
+  source /home/ops/verdi/bin/activate
+fi
+
+if [[ "$#" -eq 1  && "$@" == "supervisord" ]]; then
+  set -- supervisord -n
+else
+  if [ "${1:0:1}" = '-' ]; then
+    set -- supervisord "$@"
+  fi
+fi
+
+exec gosu $UID:$GID "$@"
