f031d9518e4d9b90ef1f730d4f93a34cd3f1f6ca
gmanipon <gmanipon@jpl.nasa.gov>
2018-09-22 01:28:42 +0000

    add pge-base

--- build_docker.sh
+++ build_docker.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 docker build --rm --force-rm -t hysds/verdi:latest -f docker/Dockerfile .
+docker build --rm --force-rm -t hysds/pge-base:latest -f docker/Dockerfile.pge-base .
---
+++ docker/pge-docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# get group id
+GID=$(id -g)
+
+# update user and group ids
+gosu 0:0 groupmod -g $GID ops 2>/dev/null
+gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
+gosu 0:0 usermod -aG docker ops 2>/dev/null
+
+# update ownership
+gosu 0:0 chown -R $UID:$GID /home/ops 2>/dev/null || true
+gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+
+# source verdi virtualenv
+if [ -e "/home/ops/verdi/bin/activate" ]; then
+  source /home/ops/verdi/bin/activate
+fi
+
+exec gosu $UID:$GID "$@"
