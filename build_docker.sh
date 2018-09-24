922289780cae5776b6b4f1399c0a3de42deba0dd
gmanipon <gmanipon@jpl.nasa.gov>
2018-09-24 15:32:00 +0000

    detect updates for cache invalidation

--- build_docker.sh
+++ build_docker.sh
@@ -7,5 +7,5 @@ if [ "$#" -ne 1 ]; then
 fi
 TAG=$1
 
-docker build --no-cache --rm --force-rm -t hysds/verdi:${TAG} -f docker/Dockerfile . || exit 1
-docker build --no-cache --rm --force-rm -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base . || exit 1
+docker build --rm --force-rm -t hysds/verdi:${TAG} -f docker/Dockerfile . || exit 1
+docker build --rm --force-rm -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base . || exit 1
