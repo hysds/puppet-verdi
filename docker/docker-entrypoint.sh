e2cde75bafc8e364dcb7e635a597232bdb01ddbf
gmanipon <gmanipon@jpl.nasa.gov>
2018-10-12 15:34:24 +0000

    set default supervisord conf with includes for adaptation; configure and run sshd from supervisord
    Factor out inet_http_server for simple override.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 # get group id
 GID=$(id -g)
 
+# generate ssh keys
+gosu 0:0 ssh-keygen -A > 2>/dev/null
+
 # update user and group ids
 gosu 0:0 groupmod -g $GID ops 2>/dev/null
 gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
