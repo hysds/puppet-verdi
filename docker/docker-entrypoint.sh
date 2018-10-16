f58c05c2e66932e0014e2b96060f8653a047af5a
Gerald Manipon <geraldjohn.m.manipon@jpl.nasa.gov>
2018-10-16 10:47:49 -0700

    extract work dir with privileged permissions

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 # get group id
 GID=$(id -g)
@@ -17,17 +17,19 @@ gosu 0:0 chown -R $UID:$GID /home/ops 2>/dev/null || true
 gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
 gosu 0:0 chown -R $UID:$GID /var/log/supervisor 2>/dev/null || true
 
+# unpack work dir style
+if [ ! -d "/data/work/.index-style" ]; then
+  gosu 0:0 mkdir -p /data/work
+  gosu 0:0 tar -C /data/work -xjf /home/ops/verdi/src/beefed-autoindex-open_in_new_win.tbz2
+  gosu 0:0 chown -R $UID:$GID /data/work 2>/dev/null || true
+fi
+
 # source verdi virtualenv
 if [ -e "/home/ops/verdi/bin/activate" ]; then
   source /home/ops/verdi/bin/activate
 fi
 
 # extract beefed autoindex
-if [ ! -d "/data/work/.index-style" ]; then
-  mkdir -p /data/work
-  tar -C /data/work -xjf /home/ops/verdi/src/beefed-autoindex-open_in_new_win.tbz2
-fi
-
 if [[ "$#" -eq 1  && "$@" == "supervisord" ]]; then
   set -- supervisord -n
 else
