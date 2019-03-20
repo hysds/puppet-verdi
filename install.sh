ac727b44fabfaee32563b79e36a604fe4d8c7dac
Gerald Manipon <pymonger@gmail.com>
2019-03-20 19:42:24 +0000

    use master branch (python3)

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ site_pp=$mod_dir/site.pp
 
 # check that module is here; if not, export it
 if [ ! -d $mod_dir ]; then
-  $git_cmd clone -b python3 --single-branch $git_loc $mod_dir
+  $git_cmd clone $git_loc $mod_dir
 fi
 
 
@@ -79,7 +79,7 @@ site_pp=$mod_dir/site.pp
 
 # check that module is here; if not, export it
 if [ ! -d $mod_dir ]; then
-  $git_cmd clone -b python3 --single-branch $git_loc $mod_dir
+  $git_cmd clone $git_loc $mod_dir
 fi
 
 
@@ -93,7 +93,7 @@ site_pp=$mod_dir/site.pp
 
 # check that module is here; if not, export it
 if [ ! -d $mod_dir ]; then
-  $git_cmd clone -b python3 --single-branch $git_loc $mod_dir
+  $git_cmd clone $git_loc $mod_dir
 fi
 
 
@@ -121,7 +121,7 @@ site_pp=$mod_dir/site.pp
 
 # check that module is here; if not, export it
 if [ ! -d $mod_dir ]; then
-  $git_cmd clone -b python3 --single-branch $git_loc $mod_dir
+  $git_cmd clone $git_loc $mod_dir
 fi
 
 
