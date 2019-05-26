ea688d9338f98d18ea7bd595424b44fd47bcdbb3
Gerald Manipon <pymonger@gmail.com>
2019-05-26 17:15:05 +0000

    install hysds release using hysds-framework

--- build_docker.sh
+++ build_docker.sh
@@ -7,6 +7,6 @@ if [ "$#" -ne 1 ]; then
 fi
 TAG=$1
 
-docker build --rm --force-rm -t hysds/verdi:${TAG} -f docker/Dockerfile . || exit 1
-docker build --rm --force-rm -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base . || exit 1
-docker build --rm --force-rm -t hysds/cuda-pge-base:${TAG} -f docker/Dockerfile.cuda-pge-base . || exit 1
+docker build --rm --force-rm -t hysds/verdi:${TAG} -f docker/Dockerfile --build-arg RELEASE=${TAG} . || exit 1
+docker build --rm --force-rm -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base --build-arg RELEASE=${TAG} . || exit 1
+docker build --rm --force-rm -t hysds/cuda-pge-base:${TAG} -f docker/Dockerfile.cuda-pge-base --build-arg RELEASE=${TAG} . || exit 1
