92acae60efa052b81b1256eb2564541b4b14f605
Gerald Manipon <pymonger@gmail.com>
2019-05-26 17:38:35 +0000

    install hysds using hysds-framework

--- templates/install_hysds.sh
+++ templates/install_hysds.sh
@@ -1,56 +1,24 @@
 #!/bin/bash
+BASE_PATH=$(dirname "${BASH_SOURCE}")
+BASE_PATH=$(cd "${BASE_PATH}"; pwd)
 
-VERDI_DIR=<%= @verdi_dir %>
-WORK_DIR=<%= @work_dir %>
+
+# usage
+usage() {
+  echo "usage: $0 <release tag>" >&2
+}
 
 
-# create virtualenv if not found
-if [ ! -e "$VERDI_DIR/bin/activate" ]; then
-  /opt/conda/bin/virtualenv --system-site-packages $VERDI_DIR
-  echo "Created virtualenv at $VERDI_DIR."
+# check usage
+if [ $# -ne 1 ]; then
+  usage
+  exit 1
 fi
+release=$1
 
 
-# source virtualenv
-source $VERDI_DIR/bin/activate
-
-
-# install latest pip and setuptools
-pip install -U pip
-pip install -U setuptools
-
-
-# force install supervisor
-if [ ! -e "$VERDI_DIR/bin/supervisord" ]; then
-  #pip install --ignore-installed supervisor
-  pip install --ignore-installed git+https://github.com/Supervisor/supervisor
-fi
-
-
-# extract beefed autoindex
-if [ ! -d "$WORK_DIR/.index-style" ]; then
-  cd $WORK_DIR
-  tar xvfj $VERDI_DIR/src/beefed-autoindex-open_in_new_win.tbz2
-fi
-
-
-# create etc directory
-if [ ! -d "$VERDI_DIR/etc" ]; then
-  mkdir $VERDI_DIR/etc
-fi
-
-
-# create log directory
-if [ ! -d "$VERDI_DIR/log" ]; then
-  mkdir $VERDI_DIR/log
-fi
-
-
-# create run directory
-if [ ! -d "$VERDI_DIR/run" ]; then
-  mkdir $VERDI_DIR/run
-fi
-
+# print out commands and exit on any errors
+set -ex
 
 # set oauth token
 OAUTH_CFG="$HOME/.git_oauth_token"
@@ -62,92 +30,30 @@ else
 fi
 
 
-# create ops directory
-OPS="$VERDI_DIR/ops"
-if [ ! -d "$OPS" ]; then
-  mkdir $OPS
-fi
-
-
-# export latest prov_es package
-cd $OPS
-PACKAGE=prov_es
-if [ ! -d "$OPS/$PACKAGE" ]; then
+# clone hysds-framework
+cd $HOME
+PACKAGE=hysds-framework
+if [ ! -d "$HOME/$PACKAGE" ]; then
   git clone ${GIT_URL}/hysds/${PACKAGE}.git
 fi
-cd $OPS/$PACKAGE
-pip install -e .
-if [ "$?" -ne 0 ]; then
-  echo "Failed to run 'pip install -e .' for $PACKAGE."
-  exit 1
+cd $HOME/$PACKAGE
+if [ "$release" = "develop" ]; then
+  ./install.sh -d verdi
+else
+  ./install.sh -r $release verdi
 fi
 
 
-# export latest osaka package
-cd $OPS
-GITHUB_REPO=osaka
-PACKAGE=osaka
-if [ ! -d "$OPS/$PACKAGE" ]; then
-  git clone ${GIT_URL}/hysds/${PACKAGE}.git
-fi
-cd $OPS/$PACKAGE
-pip install -U pyasn1
-pip install -U pyasn1-modules
-pip install -U python-dateutil
-pip install -e .
-if [ "$?" -ne 0 ]; then
-  echo "Failed to run 'pip install -e .' for $PACKAGE."
-  exit 1
-fi
+VERDI_DIR=<%= @verdi_dir %>
+WORK_DIR=<%= @work_dir %>
 
 
-# export latest hysds_commons package
-cd $OPS
-PACKAGE=hysds_commons
-if [ ! -d "$OPS/$PACKAGE" ]; then
-  git clone ${GIT_URL}/hysds/${PACKAGE}.git
-fi
-cd $OPS/$PACKAGE
-pip install -e .
-if [ "$?" -ne 0 ]; then
-  echo "Failed to run 'pip install -e .' for $PACKAGE."
-  exit 1
-fi
+# source virtualenv
+source $VERDI_DIR/bin/activate
 
 
-# export latest hysds package
-cd $OPS
-PACKAGE=hysds
-if [ ! -d "$OPS/$PACKAGE" ]; then
-  git clone ${GIT_URL}/hysds/${PACKAGE}.git
-fi
-cd $OPS/$PACKAGE/third_party/celery-v4.2.1
-pip install -e .
-cd $OPS/$PACKAGE
-pip install -e .
-if [ "$?" -ne 0 ]; then
-  echo "Failed to run 'pip install -e .' for $PACKAGE."
-  exit 1
-fi
-
-
-# export latest sciflo package
-cd $OPS
-PACKAGE=sciflo
-if [ ! -d "$OPS/$PACKAGE" ]; then
-  git clone ${GIT_URL}/hysds/${PACKAGE}.git
-fi
-cd $OPS/$PACKAGE
-pip install -e .
-if [ "$?" -ne 0 ]; then
-  echo "Failed to run 'pip install -e .' for $PACKAGE."
-  exit 1
-fi
-
-
-# export latest hysds-dockerfiles package
-cd $OPS
-PACKAGE=hysds-dockerfiles
-if [ ! -d "$OPS/$PACKAGE" ]; then
-  git clone ${GIT_URL}/hysds/${PACKAGE}.git
+# extract beefed autoindex
+if [ ! -d "$WORK_DIR/.index-style" ]; then
+  cd $WORK_DIR
+  tar xvfj $VERDI_DIR/src/beefed-autoindex-open_in_new_win.tbz2
 fi
