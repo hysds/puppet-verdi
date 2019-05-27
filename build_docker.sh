12356874eed8ba0e935f4bf249e85a3a9703c208
Gerald Manipon <pymonger@gmail.com>
2019-05-27 15:00:55 +0000

    use docker build secret

--- build_docker.sh
+++ build_docker.sh
@@ -7,6 +7,21 @@ if [ "$#" -ne 1 ]; then
 fi
 TAG=$1
 
-docker build --rm --force-rm -t hysds/verdi:${TAG} -f docker/Dockerfile --build-arg RELEASE=${TAG} . || exit 1
-docker build --rm --force-rm -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base --build-arg RELEASE=${TAG} . || exit 1
-docker build --rm --force-rm -t hysds/cuda-pge-base:${TAG} -f docker/Dockerfile.cuda-pge-base --build-arg RELEASE=${TAG} . || exit 1
+
+# set oauth token to bypass github API rate limits
+OAUTH_CFG="$HOME/.git_oauth_token"
+if [ -e "$OAUTH_CFG" ]; then
+  docker build --rm --force-rm -t hysds/verdi:${TAG} -f docker/Dockerfile \
+    --build-arg RELEASE=${TAG} --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
+  docker build --rm --force-rm -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base \
+    --build-arg RELEASE=${TAG} --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
+  docker build --rm --force-rm -t hysds/cuda-pge-base:${TAG} -f docker/Dockerfile.cuda-pge-base \
+    --build-arg RELEASE=${TAG} --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
+else
+  docker build --rm --force-rm -t hysds/verdi:${TAG} -f docker/Dockerfile \
+    --build-arg RELEASE=${TAG} . || exit 1
+  docker build --rm --force-rm -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base \
+    --build-arg RELEASE=${TAG} . || exit 1
+  docker build --rm --force-rm -t hysds/cuda-pge-base:${TAG} -f docker/Dockerfile.cuda-pge-base \
+    --build-arg RELEASE=${TAG} . || exit 1
+fi
