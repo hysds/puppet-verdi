4a2c453fd79063b16c5f9dbdbd2627095b67977d
Gerald Manipon <pymonger@gmail.com>
2019-05-27 15:07:32 +0000

    generalize to handle unauthenticated builds

--- build_docker.sh
+++ build_docker.sh
@@ -14,18 +14,15 @@ export DOCKER_BUILDKIT=1
 
 # set oauth token to bypass github API rate limits
 OAUTH_CFG="$HOME/.git_oauth_token"
-if [ -e "$OAUTH_CFG" ]; then
-  docker build --rm --force-rm -t hysds/verdi:${TAG} -f docker/Dockerfile \
-    --build-arg RELEASE=${TAG} --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
-  docker build --rm --force-rm -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base \
-    --build-arg RELEASE=${TAG} --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
-  docker build --rm --force-rm -t hysds/cuda-pge-base:${TAG} -f docker/Dockerfile.cuda-pge-base \
-    --build-arg RELEASE=${TAG} --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
-else
-  docker build --rm --force-rm -t hysds/verdi:${TAG} -f docker/Dockerfile \
-    --build-arg RELEASE=${TAG} . || exit 1
-  docker build --rm --force-rm -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base \
-    --build-arg RELEASE=${TAG} . || exit 1
-  docker build --rm --force-rm -t hysds/cuda-pge-base:${TAG} -f docker/Dockerfile.cuda-pge-base \
-    --build-arg RELEASE=${TAG} . || exit 1
+if [ ! -e "$OAUTH_CFG" ]; then
+  touch $OAUTH_CFG # create empty creds file for unauthenticated builds
 fi
+
+
+# build
+docker build --rm --force-rm -t hysds/verdi:${TAG} -f docker/Dockerfile \
+  --build-arg RELEASE=${TAG} --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
+docker build --rm --force-rm -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base \
+  --build-arg RELEASE=${TAG} --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
+docker build --rm --force-rm -t hysds/cuda-pge-base:${TAG} -f docker/Dockerfile.cuda-pge-base \
+  --build-arg RELEASE=${TAG} --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
