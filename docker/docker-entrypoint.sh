395270bb9fa7b75d45ad77c6c922757087e085fa
Gerald Manipon <pymonger@gmail.com>
2019-08-25 16:59:11 -0700

    ensure HOME is set and bash profile sourced

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# set HOME explicitly
+export HOME=/home/ops
+
 # get group id
 GID=$(id -g)
 
@@ -13,20 +16,23 @@ gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
 gosu 0:0 usermod -aG docker ops 2>/dev/null
 
 # update ownership
-gosu 0:0 chown -R $UID:$GID /home/ops 2>/dev/null || true
+gosu 0:0 chown -R $UID:$GID $HOME 2>/dev/null || true
 gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
 gosu 0:0 chown -R $UID:$GID /var/log/supervisor 2>/dev/null || true
 
 # unpack work dir style
 if [ ! -d "/data/work/.index-style" ]; then
   gosu 0:0 mkdir -p /data/work
-  gosu 0:0 tar -C /data/work -xjf /home/ops/verdi/src/beefed-autoindex-open_in_new_win.tbz2
+  gosu 0:0 tar -C /data/work -xjf $HOME/verdi/src/beefed-autoindex-open_in_new_win.tbz2
   gosu 0:0 chown -R $UID:$GID /data/work 2>/dev/null || true
 fi
 
+# source bash profile
+source $HOME/.bash_profile
+
 # source verdi virtualenv
-if [ -e "/home/ops/verdi/bin/activate" ]; then
-  source /home/ops/verdi/bin/activate
+if [ -e "$HOME/verdi/bin/activate" ]; then
+  source $HOME/verdi/bin/activate
 fi
 
 # extract beefed autoindex
--- docker/pge-docker-entrypoint.sh
+++ docker/pge-docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+# set HOME explicitly
+export HOME=/home/ops
+
 # get group id
 GID=$(id -g)
 
@@ -10,12 +13,15 @@ gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
 gosu 0:0 usermod -aG docker ops 2>/dev/null
 
 # update ownership
-gosu 0:0 chown -R $UID:$GID /home/ops 2>/dev/null || true
+gosu 0:0 chown -R $UID:$GID $HOME 2>/dev/null || true
 gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
 
+# source bash profile
+source $HOME/.bash_profile
+
 # source verdi virtualenv
-if [ -e "/home/ops/verdi/bin/activate" ]; then
-  source /home/ops/verdi/bin/activate
+if [ -e "$HOME/verdi/bin/activate" ]; then
+  source $HOME/verdi/bin/activate
 fi
 
 exec gosu $UID:$GID "$@"
