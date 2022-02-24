3301beffd1180d732fac708fa371b353821127fc
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2022-02-23 16:17:12 -0800

    NSDS-1951: Initial commit of refactoring

--- build_docker.sh
+++ build_docker.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-  echo "Enter tag as arg: $0 <tag>"
-  echo "e.g.: $0 20170620"
-  echo "e.g.: $0 latest"
+if [ "$#" -ne 7 ]; then
+  echo "Usage: $0 <tag> <github org> <github repo branch> <framework branch> <hysds release> <base image tag> <base branch>"
+  echo "e.g.: $0 20170620 hysds master develop v4.0.1-beta.7 latest develop"
+  echo "e.g.: $0 latest pymonger develop develop develop develop master"
   exit 1
 fi
 TAG=$1
-
+ORG=$2
+BRANCH=$3
+FRAMEWORK_BRANCH=$4
+HYSDS_RELEASE=$5
+BASE_IMAGE_TAG=$6
+BASE_BRANCH=$7
 
 # enable docker buildkit to allow build secrets
 export DOCKER_BUILDKIT=1
@@ -21,14 +26,17 @@ fi
 
 # build
 docker build --progress=plain --rm --force-rm \
-  -t hysds/verdi:${TAG} -f docker/Dockerfile --build-arg RELEASE=${TAG} \
+  -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base \
+  --build-arg FRAMEWORK_BRANCH=${FRAMEWORK_BRANCH} \
+  --build-arg HYSDS_RELEASE=${HYSDS_RELEASE} \
+  --build-arg TAG=${BASE_IMAGE_TAG} \
+  --build-arg ORG=${ORG} \
+  --build-arg BRANCH=${BRANCH} \
+  --build-arg BASE_BRANCH=${BASE_BRANCH} \
   --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
 docker system prune -f || :
 docker build --progress=plain --rm --force-rm \
-  -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base \
-  --build-arg RELEASE=${TAG} --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
-docker system prune -f || :
-docker build --progress=plain --rm --force-rm \
-  -t hysds/cuda-pge-base:${TAG} -f docker/Dockerfile.cuda-pge-base \
-  --build-arg RELEASE=${TAG} --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
+  -t hysds/verdi:${TAG} -f docker/Dockerfile \
+  --build-arg TAG=${TAG} \
+  --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
 docker system prune -f || :
