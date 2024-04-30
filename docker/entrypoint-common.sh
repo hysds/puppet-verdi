c440e0acd932ccccea364c0822b4f68a1fcd4243
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-04-30 15:23:56 -0700

    Remove docker stuff from entrypoints

--- docker/entrypoint-common.sh
+++ docker/entrypoint-common.sh
@@ -12,7 +12,7 @@ gosu 0:0 usermod -d /tmp/${HOME} ops 2>/dev/null
 # update user and group ids
 gosu 0:0 groupmod -g $GID ops 2>/dev/null
 gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
-gosu 0:0 usermod -aG docker ops 2>/dev/null
+#gosu 0:0 usermod -aG docker ops 2>/dev/null
 
 # restore home dir
 gosu 0:0 usermod -d ${HOME} ops 2>/dev/null
@@ -29,4 +29,4 @@ gosu 0:0 chown -R $UID:$GID $HOME/verdi/log 2>/dev/null || true
 gosu 0:0 chown -R $UID:$GID $HOME/verdi/run 2>/dev/null || true
 
 # update ownership of other files
-gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+#gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
