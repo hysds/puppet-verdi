b4686f12d4dd0acc24f0368461abaaa7f1ba94b5
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-05-02 16:06:54 -0700

    added conditional statements to entrypoint commands specific to docker

--- docker/entrypoint-common.sh
+++ docker/entrypoint-common.sh
@@ -12,7 +12,11 @@ gosu 0:0 usermod -d /tmp/${HOME} ops 2>/dev/null
 # update user and group ids
 #gosu 0:0 groupmod -g $GID ops 2>/dev/null
 #gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
-#gosu 0:0 usermod -aG docker ops 2>/dev/null
+
+if id -u "docker" >/dev/null 2>&1; then
+  gosu 0:0 usermod -aG docker ops 2>/dev/null
+fi
+
 
 # restore home dir
 gosu 0:0 usermod -d ${HOME} ops 2>/dev/null
@@ -29,4 +33,6 @@ gosu 0:0 chown -R $UID:$GID $HOME/verdi/log 2>/dev/null || true
 gosu 0:0 chown -R $UID:$GID $HOME/verdi/run 2>/dev/null || true
 
 # update ownership of other files
-#gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+if [ -f /var/run/docker.sock ]; then
+  gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+fi
