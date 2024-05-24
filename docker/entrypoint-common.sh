055b0c9cfe4dbae19b32f6f142191a0912c7a02c
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-05-24 13:22:55 -0700

    Add ability to run apachectl; remove un-needed port mappings

--- docker/entrypoint-common.sh
+++ docker/entrypoint-common.sh
@@ -38,4 +38,12 @@ if [ -e /var/run/docker.sock ]; then
   gosu 0:0 groupmod -g $GID ops 2>/dev/null
   gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
   gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+else
+  # Assume podman
+  # We need to give sudo priviliges to start up httpd to the host user
+  if [[ ! -z "$HOST_USER" ]]; then
+    gosu 0:0 su root -c "chmod u+w /etc/sudoers.d/90-cloudimg-ops"
+    gosu 0:0 su root -c "echo '${HOST_USER} ALL=NOPASSWD: /usr/sbin/apachectl' >> /etc/sudoers.d/90-cloudimg-ops"
+    gosu 0:0 su root -c "chmod u-w /etc/sudoers.d/90-cloudimg-ops"
+  fi
 fi
