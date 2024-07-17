86002a929695049e09a6067ac6a5017a723f61ba
Michael Cayanan <42812746+mcayanan@users.noreply.github.com>
2024-07-17 08:07:15 -0700

    HC-522: Update building of Verdi container to support single container with both docker and podman installed (#9)
    * HC-522: Use base container that does not set user ops
    
    * HC-522: Use base container that does not set user ops
    
    * set user home; test
    
    * update
    
    * add podman to pge-base
    
    * update
    
    * fix typo
    
    * update
    
    * make it develop
    
    * update
    
    * update
    
    * update
    
    * update
    
    * append to file
    
    * dont remove
    
    * update
    
    * update
    
    * re-order install
    
    * add allow-erasing flag
    
    * update
    
    * updates
    
    * update
    
    * Update the subuid and subgid
    
    * overwrite it
    
    * revert
    
    * revert
    
    * update
    
    * Remove docker stuff from entrypoints
    
    * update
    
    * update
    
    * fix typo
    
    * Removed updating of user and group ids from entrypoint-common
    
    * Add celeryconfig
    
    * added conditional statements to entrypoint commands specific to docker
    
    * Use -e instead of -f since docker.sock is a symlink; add back usermod/groupmod to run httpd under sudo
    
    * Add ability to run apachectl; remove un-needed port mappings
    
    * remove comments
    
    * update
    
    * remove kludges
    
    * remove more kludges
    
    * remove comment
    
    ---------
    
    Co-authored-by: Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>

--- docker/entrypoint-common.sh
+++ docker/entrypoint-common.sh
@@ -10,9 +10,12 @@ GID=$(id -g)
 gosu 0:0 usermod -d /tmp/${HOME} ops 2>/dev/null
 
 # update user and group ids
-gosu 0:0 groupmod -g $GID ops 2>/dev/null
-gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
-gosu 0:0 usermod -aG docker ops 2>/dev/null
+if [ -e /var/run/docker.sock ]; then
+  # These groupmod/usermod commands are needed in order to start up httpd under sudo
+  gosu 0:0 groupmod -g $GID ops 2>/dev/null
+  gosu 0:0 usermod -u $UID -g $GID ops 2>/dev/null
+  gosu 0:0 usermod -aG docker ops 2>/dev/null
+fi
 
 # restore home dir
 gosu 0:0 usermod -d ${HOME} ops 2>/dev/null
@@ -29,4 +32,14 @@ gosu 0:0 chown -R $UID:$GID $HOME/verdi/log 2>/dev/null || true
 gosu 0:0 chown -R $UID:$GID $HOME/verdi/run 2>/dev/null || true
 
 # update ownership of other files
-gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+if [ -e /var/run/docker.sock ]; then
+  gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
+else
+  # Assume podman
+  # We need to give sudo priviliges to start up httpd to the host user
+  if [[ ! -z "$HOST_USER" ]]; then
+    gosu 0:0 su root -c "chmod u+w /etc/sudoers.d/90-cloudimg-ops"
+    gosu 0:0 su root -c "echo '${HOST_USER} ALL=NOPASSWD: /usr/sbin/apachectl' >> /etc/sudoers.d/90-cloudimg-ops"
+    gosu 0:0 su root -c "chmod u-w /etc/sudoers.d/90-cloudimg-ops"
+  fi
+fi
