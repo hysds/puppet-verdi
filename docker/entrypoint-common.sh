1048ce6bad524012b228bdcb5b7f6471740bdf42
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-09-09 10:10:47 -0700

    Only run chown statements for docker

--- docker/entrypoint-common.sh
+++ docker/entrypoint-common.sh
@@ -20,19 +20,18 @@ fi
 # restore home dir
 gosu 0:0 usermod -d ${HOME} ops 2>/dev/null
 
-# update ownership of home dir and hidden files/dirs
-gosu 0:0 chown $UID:$GID $HOME 2>/dev/null || true
-gosu 0:0 chown -R $UID:$GID $HOME/.[!.]* 2>/dev/null || true
-
-# update ownership of verdi files/dirs
-gosu 0:0 chown $UID:$GID $HOME/verdi 2>/dev/null || true
-gosu 0:0 chown $UID:$GID $HOME/verdi/ops 2>/dev/null || true
-gosu 0:0 chown -R $UID:$GID $HOME/verdi/etc 2>/dev/null || true
-gosu 0:0 chown -R $UID:$GID $HOME/verdi/log 2>/dev/null || true
-gosu 0:0 chown -R $UID:$GID $HOME/verdi/run 2>/dev/null || true
-
 # update ownership of other files
 if [ -e /var/run/docker.sock ]; then
+  # update ownership of home dir and hidden files/dirs
+  gosu 0:0 chown $UID:$GID $HOME 2>/dev/null || true
+  gosu 0:0 chown -R $UID:$GID $HOME/.[!.]* 2>/dev/null || true
+
+  # update ownership of verdi files/dirs
+  gosu 0:0 chown $UID:$GID $HOME/verdi 2>/dev/null || true
+  gosu 0:0 chown $UID:$GID $HOME/verdi/ops 2>/dev/null || true
+  gosu 0:0 chown -R $UID:$GID $HOME/verdi/etc 2>/dev/null || true
+  gosu 0:0 chown -R $UID:$GID $HOME/verdi/log 2>/dev/null || true
+  gosu 0:0 chown -R $UID:$GID $HOME/verdi/run 2>/dev/null || true
   gosu 0:0 chown -R $UID:$GID /var/run/docker.sock 2>/dev/null || true
 else
   # Assume podman
