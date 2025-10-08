9e4d430d3098fe4ae9e5d4832a2f240de1b30ed4
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2025-10-08 11:34:23 -0700

    Wire out being able to specify a base and dev container with a tag different than the final tag

--- build_docker.sh
+++ build_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$#" -ne 7 ]; then
-  echo "Usage: $0 <tag> <github org> <github repo branch> <framework branch> <hysds release> <base image tag> <base branch>"
-  echo "e.g.: $0 20170620 hysds master develop v4.0.1-beta.7 latest develop"
-  echo "e.g.: $0 latest pymonger develop develop develop develop master"
+  echo "Usage: $0 <tag> <github org> <github repo branch> <framework branch> <hysds release> <base image tag> <final tag> <base branch>"
+  echo "e.g.: $0 20170620 hysds master develop v4.0.1-beta.7 latest latest develop"
+  echo "e.g.: $0 latest pymonger develop develop develop develop develop master"
   exit 1
 fi
 TAG=$1
@@ -11,7 +11,8 @@ BRANCH=$3
 FRAMEWORK_BRANCH=$4
 HYSDS_RELEASE=$5
 BASE_IMAGE_TAG=$6
-BASE_BRANCH=$7
+FINAL_TAG=$7
+BASE_BRANCH=$8
 
 
 # enable docker buildkit to allow build secrets
@@ -26,18 +27,20 @@ fi
 
 
 # build
+# ./build_docker.sh 20251008 hysds docker HC-589 develop HC-589 develop
 docker build --progress=plain --rm --force-rm \
   -t hysds/pge-base:${TAG} -f docker/Dockerfile.pge-base \
   --build-arg FRAMEWORK_BRANCH=${FRAMEWORK_BRANCH} \
   --build-arg HYSDS_RELEASE=${HYSDS_RELEASE} \
-  --build-arg TAG=${BASE_IMAGE_TAG} \
+  --build-arg BASE_IMAGE_TAG=${BASE_IMAGE_TAG} \
+  --build-arg FINAL_TAG=${FINAL_TAG} \
   --build-arg ORG=${ORG} \
   --build-arg BRANCH=${BRANCH} \
   --build-arg BASE_BRANCH=${BASE_BRANCH} \
   --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
 docker system prune -f || :
 docker build --progress=plain --rm --force-rm \
-  -t hysds/verdi:${TAG} -f docker/Dockerfile \
-  --build-arg TAG=${TAG} \
+  -t hysds/verdi:${FINAL_TAG} -f docker/Dockerfile \
+  --build-arg TAG=${FINAL_TAG} \
   --secret id=git_oauth_token,src=$OAUTH_CFG . || exit 1
 docker system prune -f || :
